d8898d3bee2ba8085dc0a392a8f31f32d0a6c531
Christian Bunk <christian.bunk@sensorberg.com>
2022-04-26 11:39:03 +0200

    remove cache; set refreh Interval to 30 min

--- app/src/main/java/com/bunk/uvindex/api/WeatherRepository.kt
+++ app/src/main/java/com/bunk/uvindex/api/WeatherRepository.kt
@@ -1,31 +1,18 @@
 package com.bunk.uvindex.api
 
-import com.bunk.uvindex.Cache
 import timber.log.Timber
 
 class WeatherRepository(
 	private val openWeatherMapApi: OpenWeatherMapApi,
 ) {
 
-	// TODO does not take location into consideration...
-	private val cache: Cache<WeatherData> = Cache()
-
-	fun last(): Cache<WeatherData> = cache
-
 	suspend fun getWeather(
 		latitude: Double,
 		longitude: Double,
 	): WeatherData? {
 		Timber.d("getWeather")
-		if (cache.isStale()) {
-			val weatherData: WeatherData? = openWeatherMapApi.getWeather(latitude = latitude, longitude = longitude).dataOrNull
-			Timber.d("cache stale")
-			Timber.d("weatherData: $weatherData")
-			if (weatherData != null) cache.value = weatherData
-		} else {
-			Timber.d("cache not stale")
+		return openWeatherMapApi.getWeather(latitude = latitude, longitude = longitude).dataOrNull?.also {
+			Timber.d("weather: $it")
 		}
-
-		return cache.value
 	}
 }
