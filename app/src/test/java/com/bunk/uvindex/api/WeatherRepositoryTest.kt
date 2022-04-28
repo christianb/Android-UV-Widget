8f27d65e2395d36097f78997524157f8c988a28b
Christian Bunk <christian.bunk@sensorberg.com>
2022-04-28 21:46:13 +0200

    run unit tests with github actions

---
+++ app/src/test/java/com/bunk/uvindex/api/WeatherRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.bunk.uvindex.api
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class WeatherRepositoryTest {
+
+	@Test
+	fun foo() {
+		assertTrue(true)
+	}
+
+	@Test
+	fun bar() {
+		assertTrue(false)
+	}
+}
